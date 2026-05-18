6abd5d322a8804a4205c44292f5ff5c733ca6820
Bin <linzheyu17@gmail.com>
2026-05-19 00:46:59 +0800

    5 commit

--- services.py
+++ services.py
@@ -1,4 +1,84 @@
 from uuid import uuid4
+
+from database import get_conn
+
+
+# ----------------
+# 房間系統
+# ----------------
+
+
+def create_room(room_name):
+    token = uuid4().hex[:8]
+
+    with get_conn() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT OR IGNORE INTO rooms(room_name, room_token) VALUES (?, ?)",
+            (room_name, token),
+        )
+
+
+    return token
+
+
+
+def get_room(room_name):
+    with get_conn() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM rooms WHERE room_name = ?",
+            (room_name,),
+        )
+
+        return cursor.fetchone()
+
+
+
+# ----------------
+# 成員系統
+# ----------------
+
+
+def add_member(room_name, name, nickname):
+    member_id = uuid4().hex
+
+    with get_conn() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO members(id, room_name, name, nickname)
+            VALUES (?, ?, ?, ?)
+            """,
+            (member_id, room_name, name, nickname),
+        )
+
+    return member_id
+
+
+
+
+def get_members(room_name):
+    with get_conn() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM members WHERE room_name = ?",
+            (room_name,),
+        )
+
+        return cursor.fetchall()
+
+
+# ----------------
+# 時間系統
+# ----------------
+
+
+def save_availability(member_id, slots):
     with get_conn() as conn:
         cursor = conn.cursor()
 
@@ -12,6 +92,7 @@ from uuid import uuid4
                 "INSERT INTO availability(member_id, slot) VALUES (?, ?)",
                 (member_id, slot),
             )
+
 
 
 
@@ -45,6 +126,7 @@ def add_norm(room_name, content):
             """,
             (norm_id, room_name, content),
         )
+
 
 
 
